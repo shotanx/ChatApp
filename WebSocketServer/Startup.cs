38e1df01849fa979adc7dd963afd2c993c0186aa
Shota <gladius.vand@gmail.com>
2020-08-12 18:52:17 +0400

    Changed the default request pipeline in Startup.cs

--- WebSocketServer/Startup.cs
+++ WebSocketServer/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Net.WebSockets;
 
 namespace WebSocketServer
 {
@@ -21,20 +22,44 @@ namespace WebSocketServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseWebSockets();
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.Use(async (context, next) =>
             {
-                endpoints.MapGet("/", async context =>
+                WriteRequestParam(context); // es metodi mogvianebit davamatet. Console logshi saintereso rameebs achvenebs
+                if (context.WebSockets.IsWebSocketRequest)
                 {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                    WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                    Console.WriteLine("WebSocket Connected");
+                }
+                else
+                {
+                    Console.WriteLine("Hello from the 2nd request delegate.");
+                    await next();
+                }
             });
+
+            //es ubralod savarjishod damatebit ragac metodi. next() ar chirdeba imitom rom amis shemdeg arc araferia.
+            app.Run(async context =>
+            {
+                Console.WriteLine("Hello from the 3rd request delegate. es mxolod serveris consoleshi chans.");
+                await context.Response.WriteAsync("Hello from the 3rd request delegate. amas achvenebs browsershi localhost:5000-ze");
+            });
+        }
+
+        //metodi romelic amobechdavs request header-s. HttpContext-idan sxvadasxva properties amovbechdavt.
+        public void WriteRequestParam(HttpContext context) // es konteqsti zemotastan arafer shuashia
+        {
+            Console.WriteLine("Request Method: " + context.Request.Method);
+            Console.WriteLine("Request Protocol: " + context.Request.Protocol);
+
+            if (context.Request.Headers != null)
+            {
+                foreach (var h in context.Request.Headers)
+                {
+                    Console.WriteLine("--> " + h.Key + " : " + h.Value);
+                }
+            }
         }
     }
 }
